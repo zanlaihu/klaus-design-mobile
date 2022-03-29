4458adeb9a9e040bdf7dfdfc3409e7afab11dd62
胡赞來 <361206768@qq.com>
2022-03-29 09:27:29 +0800

    add

---
+++ .dumi/theme/components/device.tsx
@@ -0,0 +1,46 @@
+import type { FC } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import QRCode from 'qrcode.react'
+import { context, usePrefersColor } from 'dumi/theme'
+import './device.less'
+
+interface IDeviceProps {
+  className?: string
+  url: string
+}
+
+export const Device: FC<IDeviceProps> = ({ url }) => {
+  const [renderKey, setRenderKey] = useState(Math.random())
+  const [color] = usePrefersColor()
+  const {
+    config: { mode },
+  } = useContext(context)
+
+  // re-render iframe if prefers color changed
+  useEffect(() => {
+    setRenderKey(Math.random())
+  }, [color])
+
+  return (
+    <div className={'adm-doc-device'} data-device-type='iOS' data-mode={mode}>
+      <iframe title='dumi-previewer' src={url} key={renderKey} />
+      <div className='adm-doc-device-action'>
+        <button
+          className='__dumi-default-icon'
+          role='refresh'
+          onClick={() => setRenderKey(Math.random())}
+        />
+        <button className='__dumi-default-icon' role='qrcode'>
+          <QRCode value={url} size={96} />
+        </button>
+        <a
+          href={url}
+          target='_blank'
+          rel='noreferrer'
+          className='__dumi-default-icon'
+          role='open-demo'
+        />
+      </div>
+    </div>
+  )
+}
